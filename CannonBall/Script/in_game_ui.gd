bc58ad622240cb56b8cf8f12cd5ebe2f55fcbf66
Jasontreks <jasontreks@outlook.com>
2025-07-28 23:22:02 +0900

    feat: 쉘 선택 다이얼 UI 구현

--- CannonBall/Script/in_game_ui.gd
+++ CannonBall/Script/in_game_ui.gd
@@ -17,7 +17,13 @@ class_name InGameUI
 @onready var lbP1Time: Label = $Header/Dashboard/P1Time
 @onready var lbP2Time: Label = $Header/Dashboard/P2Time
 
+## ShellDial
+@onready var spShellDial0: Sprite2D = $ShellDial/PolygonButton_Top/SP_Shell0
+@onready var spShellDial1: Sprite2D = $ShellDial/PolygonButton_Mid/SP_Shell1
+@onready var spShellDial2: Sprite2D = $ShellDial/PolygonButton_Bot/SP_Shell2
+
 var uiMgr: UIManager = null
+var game: Game = null
 
 func on_observe() -> void:
 	if not multiplayer.is_server():
@@ -93,3 +99,40 @@ func _process(delta: float) -> void:
 func set_player_life_time(p1time: float, p2time: float) -> void:
 	lbP1Time.text = "%.1f" % p1time
 	lbP2Time.text = "%.1f" % p2time
+
+
+func _on_polygon_button_top_pressed() -> void:
+	if not game:
+		return
+	if multiplayer.is_server():
+		game.players[0].selectedShell = 0
+	else:
+		game.players[1].selectedShel = 0
+
+	spShellDial0.get_child(0).visible = true
+	spShellDial1.get_child(0).visible = false
+	spShellDial2.get_child(0).visible = false
+
+func _on_polygon_button_mid_pressed() -> void:
+	if not game:
+		return
+	if multiplayer.is_server():
+		game.players[0].selectedShell = 1
+	else:
+		game.players[1].selectedShel = 1
+
+	spShellDial0.get_child(0).visible = false
+	spShellDial1.get_child(0).visible = true
+	spShellDial2.get_child(0).visible = false
+
+func _on_polygon_button_bot_pressed() -> void:
+	if not game:
+		return
+	if multiplayer.is_server():
+		game.players[0].selectedShell = 2
+	else:
+		game.players[1].selectedShel = 2
+
+	spShellDial0.get_child(0).visible = false
+	spShellDial1.get_child(0).visible = false
+	spShellDial2.get_child(0).visible = true
