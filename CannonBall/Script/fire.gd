75acd456c4882b121c311b6125b681e8bb29e8ea
Jasontreks <jasontreks@outlook.com>
2025-08-02 20:41:46 +0900

    fix

--- CannonBall/Script/fire.gd
+++ CannonBall/Script/fire.gd
@@ -10,24 +10,18 @@ func on_spawned() -> void:
 func _enter_tree() -> void:
 	game = get_parent() as Game
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	particle.one_shot = false
 	particle.emitting = true
 	
 	if multiplayer.is_server():
-		#game.regist_lifetime(self.name, 4, 0, stop)
 		game.regist_lifetime(self.name, 4, 0)
 
 @rpc("any_peer", "call_local")
 func lifetime_end() -> void:
 	particle.one_shot = true
 	particle.emitting = false
-	
-#func stop():
-	#particle.one_shot = true
-	#particle.emitting = false
-	#print("fire emitting stopped")
-	
-func _on_cpu_particles_2d_finished() -> void:
+
+func _on_gpu_particles_2d_finished() -> void:
+	print("fire effect deleted")
 	game.delete_object(self.name)
