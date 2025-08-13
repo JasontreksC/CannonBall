653b6a4bdf0f096ab9e40caa11293a0dcc830462
Jasontreks <jasontreks@outlook.com>
2025-08-13 15:42:49 +0900

    feat: 대미지필드를 틱/히트 로 분리

---
+++ CannonBall/Script/tick_damage_field.gd
@@ -0,0 +1,28 @@
+extends Node2D
+class_name TickDamageField
+
+## SERVER ONLY: 서버에서만 작동하는 객체
+var target: int = -1
+var type: int
+var xrange: XRange = XRange.new()
+
+var tickDamage: int = 0  # 틱 대미지 간격은 1초로 고정
+var tickInterval: float = 0
+var lifeturn: int = 0
+
+var game: Game = null
+@onready var timer: Timer = $Timer
+
+func activate():
+	if target != -1:
+		timer.start(tickInterval)
+
+func _enter_tree() -> void:
+	game = get_parent().get_parent() as Game
+
+func _ready() -> void:
+	pass
+	
+func _on_timer_timeout() -> void:
+	if xrange.in_range(game.players[target].global_position.x):
+		game.players[target].rpc("get_damage", tickDamage, type)
